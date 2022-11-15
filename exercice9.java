f43a88487b0c37ca65fe88e3fe8a31599cdc4b26
paola <tsaguepaola@gmail.com>
2022-11-15 17:03:41 +0100

    ajout de code

---
+++ exercice9.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.math.*;
+import java.io.File;
+
+public class exercice9{
+public static void main(String[] args) {
+        Scanner tap= new Scanner(System.in);
+    System.out.println(" entrer deux nombres: " );
+        int N=tap.nextInt();
+        }
+        }
---
+++ out/production/exo9/README.md
@@ -0,0 +1,2 @@
+# exo9
+saisir deux nombres et afficher les diviseurs communs de ceux ci
